6ca60888f5b17e991df11b70c0a70209acc471cb
Mac17369 <Mac17369@uvg.edu.gt>
2018-05-18 16:31:12 -0600

    Ultimo cambio

--- MetodosHT10.py
+++ MetodosHT10.py
@@ -28,6 +28,42 @@ def ingresarPaciente(nombre, telefono):
     print("Se ha ingresado un nuevo paciente\n")
 
 def ingresarPacienteDoc():
+    print("\nEstos son los pacientes disponibles en la base de datos: ")
+    q = 'MATCH (u: Pacientes) RETURN u'
+    paciente = db.query(q, returns=(client.Node, str, client.Node))
+    for k in paciente:
+        print(" - " + "%s" % (k[0]["nombre"]))
+    nombreP = raw_input("Ingrese el nombre del Paciente que desea relacionar: ")
+    print("\nEstos son los doctores disponibles en la base de datos: ")
+    q = 'MATCH (u: Doctores) RETURN u'
+    paciente = db.query(q, returns=(client.Node, str, client.Node))
+    for k in paciente:
+        print(" - " + "%s" % (k[0]["nombre"]))
+    nombreD = raw_input("Ingrese el nombre del Doctor que desea relacionar: ")
+    q = 'MATCH (u: Doctores) WHERE u.nombre="'+nombreD+'" RETURN u'
+    obtenidos = db.query(q, returns=(client.Node))
+    final = len(obtenidos)
+    q = 'MATCH (u: Pacientes) WHERE u.nombre="'+nombreP+'" RETURN u'
+    nodo = db.query(q, returns=(client.Node))
+    finalL = len(nodo)
+    if(final>0 and finalL>0):
+        fecha = raw_input("Fecha en la que visito al DOctor: ")
+        nombre = raw_input("Medicina preescribida: ")
+        inicio = raw_input("Inicio de tratamiento ")
+        concluye = raw_input("Final de tratamiento: ")
+        cantidad = raw_input("Dosis preescribida: ")
+        medicinaNueva = db.nodes.create(nombre=nombre,Inicio=inicio,Final=concluye,Cantidad=cantidad)
+        Drogas.add(medicinaNueva)
+        for k in obtenidos:
+            for i in nodo:
+                i[0].relationships.create("VISITS",k[0],fecha=fecha
+                i[0].relationships.create("TAKE",medicinaNueva)
+                k[0].relationships.create("PRESCRIBE",medicinaNueva)              
+        print("Relacion creada\n")
+    else:
+        print("No existe nombre")
+
+
 
 def consultaEspecialidad():
     print("Especialidades existentes: ")
@@ -40,7 +76,7 @@ def consultaEspecialidad():
             elemento = r[0]["especialidad"]
             listaEspecialidades.append(elemento)
     Consulta = raw_input("Que especialidad desea? ")
-    q = "MATCH (u: Doctores) WHERE u.especialidad="' +Consulta+ '"RETURN u"
+    q = 'MATCH (u: Doctores) WHERE u.especialidad="'+Consulta+'" RETURN u'
     especialidades = db.query(q, returns=(client.Node, str, client.Node))
     for j in especialidades:
          #print("Se encontro al Doctor(a) %s con numero de contacto %s" % (r[0]["name"],r[0]["telefono"]))
